c4fa8c6e7e9cefc326a7c851bbbab34e6e6701ec
momonoski55555 <112166484+momonoski55555@users.noreply.github.com>
2025-05-23 18:04:17 +0200

    added enemies

--- scences/player/player.gd
+++ scences/player/player.gd
@@ -10,18 +10,15 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	else:
-		
-		# Handle jump.
-		if Input.is_action_just_pressed("ui_accept") :
-			velocity.y = JUMP_VELOCITY
-		
-		# Get the input direction and handle the movement/deceleration.
-		# As good practice, you should replace UI actions with custom gameplay actions.
 		var input_dir := Input.get_vector("A", "D", "W", "S")
 		var direction: Vector3 = ($Gimble.transform.basis * Vector3(input_dir.x, velocity.y, input_dir.y)).normalized()
 		if direction:
 			velocity = direction * SPEED
 		else:
 			velocity = lerp(velocity, Vector3.ZERO, delta * 3)
-	
+		
+		if Input.is_action_just_pressed("ui_accept") and is_on_floor() :
+			velocity.y = JUMP_VELOCITY
+		
+
 	move_and_slide()
