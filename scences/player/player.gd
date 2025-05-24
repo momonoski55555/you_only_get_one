120da0c23dc03672b140244287f2e1d73d24a61c
momonoski55555 <112166484+momonoski55555@users.noreply.github.com>
2025-05-24 23:21:10 +0200

    made a basic game mechanics

--- scences/player/player.gd
+++ scences/player/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody3D
 class_name Player
 
+@onready var shape_cast_3d: ShapeCast3D = $Gimble/Camera/ShapeCast3D
 
 @onready var ray_cast_3d: RayCast3D = $Gimble/Camera/RayCast3D
 @onready var point: Node3D = $Gimble/Camera/point
@@ -12,6 +13,7 @@ const JUMP_VELOCITY = 4.5
 var bullets: int = 1
 
 func _ready() -> void:
+	%ammo_count.text = str(bullets)
 	clamp(bullets,0,1)
 
 
@@ -19,17 +21,24 @@ func _input(event: InputEvent) -> void:
 	var bullet = PLAYER_BULLET.instantiate()
 	if Input.is_action_just_released("Fire"):
 		if bullets > 0:
-			bullets -= 1
-			point.add_child(bullet)
-			bullet.top_level = true
-		else:
+			#bullets -= 1
+			#point.add_child(bullet)
+			#bullet.top_level = true
+			#%ammo_count.text = str(bullets)
+		#else:
 			melee_attack()
 
 func add_bullet() -> void: 
 	bullets += 1
+	%ammo_count.text = str(bullets)
 
 func melee_attack() -> void:
 	print("melee hit")
+	var collided = ray_cast_3d.get_collider()
+	if collided:
+		if collided.name == "Enemy":
+			print("het")
+			collided.queue_free()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
